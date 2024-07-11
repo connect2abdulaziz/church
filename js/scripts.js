e92a8195f56b7a9667b2ec9d0bfc30b15111effe
Abdul Aziz <connect2abdulaziz@gmail.com>
2024-07-11 16:50:41 +0500

    Add files via upload
    Church Website

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
+});
+
+const navItems = document.querySelectorAll('.nav-item');
+navItems.forEach(item => {
+    item.addEventListener('click', () => {
+        navItems.forEach(nav => nav.classList.remove('active'));
+        item.classList.add('active');
+    });
+});
+
