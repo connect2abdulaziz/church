4c55019e33efb63d46319c98f86563cf837ee522
Abdul Aziz <connect2abdulaziz@gmail.com>
2024-07-15 10:21:15 +0500

    # On branch v3 # Changes to be committed: #	modified:   css/styles.css #	deleted:    fonts/LibreBaskerville-Bold.woff #	deleted:    fonts/LibreBaskerville-Bold.woff2 #	deleted:    fonts/LibreBaskerville-Italic.woff #	deleted:    fonts/LibreBaskerville-Italic.woff2 #	deleted:    fonts/LibreBaskerville-Regular.woff #	deleted:    fonts/LibreBaskerville-Regular.woff2 #	modified:   index.html #	modified:   js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,15 @@
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav-links");
 
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('.nav-links');
-
-burger.addEventListener('click', () => {
-    nav.classList.toggle('nav-active');
-    burger.classList.toggle('toggle');
+burger.addEventListener("click", () => {
+  nav.classList.toggle("nav-active");
+  burger.classList.toggle("toggle");
 });
 
-const navItems = document.querySelectorAll('.nav-item');
-navItems.forEach(item => {
-    item.addEventListener('click', () => {
-        navItems.forEach(nav => nav.classList.remove('active'));
-        item.classList.add('active');
-    });
+const navItems = document.querySelectorAll(".nav-item");
+navItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    navItems.forEach((nav) => nav.classList.remove("active"));
+    item.classList.add("active");
+  });
 });
-
